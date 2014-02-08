2451e1a792d2064c2ea52ef4aa0e74947a2fb2ba
Mike Swider <mikeswider37@gmail.com>
2014-02-08 11:25:00 -0500

    server config

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ config = {
     // ### Development **(default)**
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
-        url: 'mikeswider.dev',
+        url: 'http://mikeswider.dev',
 
         // Example mail config
         // Visit http://docs.ghost.org/mail for instructions
@@ -34,7 +34,7 @@ config = {
         },
         server: {
             // Host to be passed to node's `net.Server#listen()`
-            host: 'mikeswider.dev',
+            host: '127.0.0.1',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
             port: '80'
         }
@@ -44,7 +44,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: 'http://mikeswider.com',
         mail: {},
         database: {
             client: 'sqlite3',
@@ -57,7 +57,7 @@ config = {
             // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: '2368'
+            port: '80'
         }
     },
 
